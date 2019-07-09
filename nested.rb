3bcdc745787fc30e72273f5b1cc37a4847ea8a67
mariah <mariah@gardziola.com>
2019-07-09 14:33:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -93,7 +93,7 @@ def adding_matz
      }
      
 programmer_hash[:yukihiro_matsumoto] = :known_for
-programmer_hash[:yukihiro_matsumoto] = :languages]
+programmer_hash[:yukihiro_matsumoto] = :languages
 
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
