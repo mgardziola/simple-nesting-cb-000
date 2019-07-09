a0dc4cfe3a2c17d9655da9c4e20830ae4535de43
mariah <mariah@gardziola.com>
2019-07-09 14:30:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -117,7 +117,7 @@ def changing_alan
      
      alans_new_info = "GUI"
      
-      [:alan_kay][:known_for] = alans_new_info
+      programmer_hash[:alan_kay][:known_for] = alans_new_info
 
      
 end
